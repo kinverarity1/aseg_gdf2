2ce6701e6fb397c7ebd866b8eb43645742e2de8d
Kent Inverarity <kinverarity@hotmail.com>
2018-07-24 22:15:22 +0930

    Work on getting field data available

--- aseg_gdf2/__init__.py
+++ aseg_gdf2/__init__.py
@@ -1,13 +1,9 @@
 import logging
-import re
 import os
+import re
 
+import dask.dataframe as dd
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm, colors
-from matplotlib.patches import Rectangle
-from matplotlib.collections import PatchCollection
 
 logger = logging.getLogger(__name__)
 
@@ -44,12 +40,12 @@ def read(filename):
 
 
 class GDF2(object):
-    def __init__(self, dfn, dat_fn):
+    def __init__(self, dfn, dat_fn, **kws):
         self.dfn = dfn
-        self.dat_fn = dat_fn
+        kws['dfn'] = dfn
+        self.dat = DAT(dat_fn, **kws)
     
-
-
+    
 
 class DFN(object):
     def __init__(self, filename=None, content=None):
@@ -120,9 +116,64 @@ class DFN(object):
                             f['comment'] = chunk
                     else:
                         f['format'] = remaining
+                    m = re.match('([0-9]*)([a-zA-Z]{1})([0-9]+)', f['format'])
+                    if m:
+                        n = m.group(1)
+                        try:
+                            n = int(n)
+                        except ValueError:
+                            n = 1
+                        f['shape'] = (n, )
+                    else:
+                        f['shape'] = (1, )
                     
                     logger.info('line {}: adding field {} to record type RT={}'.format(i, str(f), rt))
                     self.record_types[rt]['fields'].append(f)
                     if f['name'] == 'RT':
                         self.record_types[rt]['format'] = f['format']
+
+    @property
+    def null_fields(self):
+        return tuple([f['name'] for f in self.record_types['']['fields']])
+
+
+
+class DAT(object):
+    '''Represent GDF2 data file (.dat).
+
+    Args:
+        filename (str): path of .dat file
+        method (str): either 'delimited' - others to come. If
+            'delimited' then ``kws['delimiter'] = '\s+'`` will
+            be included for pandas.read_table
+        **kws: keyword arguments for pandas.read_table
+
+    '''
+    def __init__(self, filename, method='delimited', dfn=None, **kws):
+        self.dfn = dfn
+        self.funcs = {}
+        self.filename = filename
+        self.method = method
+        if self.method == 'delimited':
+            if not 'delimiter' in kws:
+                kws['delimiter'] = '\s+'
+            self.kws = kws
+            self.refresh()
+
+    def refresh(self):
+        self.df = dd.read_table(self.filename, **self.kws)
+
+    def null_field(self, name):
+        for i, f in enumerate(self.dfn.record_types['']['fields']):
+            s = sum([f['shape'][0] for f in self.dfn.record_types['']['fields'][:i]])
+            if f['name'] == name:
+                field_data = self.df.iloc[:, s: s + f['shape'][0]].copy()
+                if field_data.shape[1] == 1:
+                    return field_data.iloc[:, 0].values
+                else:
+                    return field_data.values
+    
+    def null_fields(self):
+        for f in self.dfn.record_types['']['fields']:
+            yield self.null_field(f['name'])
 
