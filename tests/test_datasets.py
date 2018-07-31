713f76d0eb91e79f724de54c1628c3c724954ef9
Kent Inverarity <kinverarity@hotmail.com>
2018-07-31 21:55:53 +0930

    add regression test for #6

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -37,3 +37,15 @@ def test_field_data():
 def test_field_null():
     gdf = aseg_gdf2.read(dset('9a13704a', 'Mugrave_WB_MGA52'))
     assert np.isnan(gdf.get_field('Con_doi')[5, -6])
+
+def test_df_usecols():
+    gdf = aseg_gdf2.read(dset('9a13704a', 'Mugrave_WB_MGA52'))
+    df = gdf.df(usecols=['LINE', 'RUnc', ])
+    assert (df.columns.values == [
+        'LINE', 'RUnc[0]', 'RUnc[1]', 'RUnc[2]', 'RUnc[3]', 'RUnc[4]',
+        'RUnc[5]', 'RUnc[6]', 'RUnc[7]', 'RUnc[8]', 'RUnc[9]', 'RUnc[10]',
+        'RUnc[11]', 'RUnc[12]', 'RUnc[13]', 'RUnc[14]', 'RUnc[15]',
+        'RUnc[16]', 'RUnc[17]', 'RUnc[18]', 'RUnc[19]', 'RUnc[20]',
+        'RUnc[21]', 'RUnc[22]', 'RUnc[23]', 'RUnc[24]', 'RUnc[25]',
+        'RUnc[26]', 'RUnc[27]', 'RUnc[28]', 'RUnc[29]'
+    ]).all()
