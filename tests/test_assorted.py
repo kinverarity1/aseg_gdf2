e95b17c0005ff87131a8d47f9101de607887fc99
Kent Inverarity <kinverarity@hotmail.com>
2019-10-21 12:10:10 +1030

    Update regression tests for new API

--- tests/test_assorted.py
+++ tests/test_assorted.py
@@ -1,8 +1,9 @@
 import os, sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+from pathlib import Path
 
-here = lambda *args: os.path.join(os.path.dirname(__file__), *args)
+repo = Path(__file__).parent.parent
 
 import pytest
 
@@ -10,28 +11,43 @@ import aseg_gdf2
 
 
 def test_read_ext_dfn():
-    gdf = aseg_gdf2.read(here("aseg_examples", "Example_AeroMag_MuppetTown_2009.dfn"))
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "aseg_examples" / "Example_AeroMag_MuppetTown_2009.dfn")
+    )
 
 
 def test_read_ext_des():
-    gdf = aseg_gdf2.read(here("aseg_examples", "Example_AeroMag_MuppetTown_2009.des"))
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "aseg_examples" / "Example_AeroMag_MuppetTown_2009.des")
+    )
 
 
 def test_read_ext_dat():
-    gdf = aseg_gdf2.read(here("aseg_examples", "Example_AeroMag_MuppetTown_2009.dat"))
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "aseg_examples" / "Example_AeroMag_MuppetTown_2009.dat")
+    )
 
 
 def test_read_no_ext():
-    gdf = aseg_gdf2.read(here("aseg_examples", "Example_AeroMag_MuppetTown_2009"))
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "aseg_examples" / "Example_AeroMag_MuppetTown_2009")
+    )
 
 
 def test_read_incorrect():
     with pytest.raises(OSError):
         gdf = aseg_gdf2.read(
-            here("aseg_examples", "Example_AeroMag_MuppetTown_2009.data")
+            str(
+                repo
+                / "tests"
+                / "aseg_examples"
+                / "Example_AeroMag_MuppetTown_2009.data"
+            )
         )
 
 
 def test_find_dat_file():
-    gdf = aseg_gdf2.read(here("aseg_examples", "Example_AeroMag_MuppetTown_2009"))
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "aseg_examples" / "Example_AeroMag_MuppetTown_2009")
+    )
     assert gdf.dat_filename.lower().endswith("example_aeromag_muppettown_2009.dat")
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,7 +2,9 @@ import os, sys
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-dset = lambda *args: os.path.join(os.path.dirname(__file__), "example_datasets", *args)
+from pathlib import Path
+
+repo = Path(__file__).parent.parent
 
 import pytest
 import numpy as np
@@ -11,44 +13,55 @@ import aseg_gdf2
 
 
 def test_iterrows_1():
-    gdf = aseg_gdf2.read(dset("3bcfc711", "GA1286_Waveforms"))
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "example_datasets" / "3bcfc711" / "GA1286_Waveforms")
+    )
     for row in gdf.iterrows():
         assert row["Time"] == 0.0052
         break
 
 
 def test_iterrows_2():
-    gdf = aseg_gdf2.read(dset("3bcfc711", "GA1286_Waveforms"))
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "example_datasets" / "3bcfc711" / "GA1286_Waveforms")
+    )
     for row in gdf.iterrows():
         pass
     assert row["Time"] == 59.9948
 
 
-def test_chunksize():
-    gdf = aseg_gdf2.read(dset("3bcfc711", "GA1286_Waveforms"))
-    lengths = []
-    for chunk in gdf.df_chunked(chunksize=10000):
-        lengths.append(len(chunk))
-    assert lengths == [10000, 10000, 3040]
-
-
-def test_df():
-    gdf = aseg_gdf2.read(dset("3bcfc711", "GA1286_Waveforms"))
+def test_length_df():
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "example_datasets" / "3bcfc711" / "GA1286_Waveforms")
+    )
     assert len(gdf.df()) == 23040
 
 
-def test_field_data():
-    gdf = aseg_gdf2.read(dset("9a13704a", "Mugrave_WB_MGA52"))
-    assert gdf.get_field("Con_doi")[4, -6] == [174.27675]
+def test_nrecords():
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "example_datasets" / "3bcfc711" / "GA1286_Waveforms")
+    )
+    assert gdf.nrecords == 23040
 
 
-def test_field_null():
-    gdf = aseg_gdf2.read(dset("9a13704a", "Mugrave_WB_MGA52"))
-    assert np.isnan(gdf.get_field("Con_doi")[5, -6])
+def test_2d_field_data():
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "example_datasets" / "9a13704a" / "Mugrave_WB_MGA52")
+    )
+    assert gdf.get_field_data("Con_doi")[4, -6] == [174.27675]
 
 
-def test_df_usecols():
-    gdf = aseg_gdf2.read(dset("9a13704a", "Mugrave_WB_MGA52"))
+def test_2d_field_null():
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "example_datasets" / "9a13704a" / "Mugrave_WB_MGA52")
+    )
+    assert np.isnan(gdf.get_field_data("Con_doi")[5, -6])
+
+
+def test_df_2d_usecols():
+    gdf = aseg_gdf2.read(
+        str(repo / "tests" / "example_datasets" / "9a13704a" / "Mugrave_WB_MGA52")
+    )
     df = gdf.df(usecols=["LINE", "RUnc"])
     assert (
         df.columns.values
