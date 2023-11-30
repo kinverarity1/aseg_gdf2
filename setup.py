56bb34bfcdba3623e4183274961198efc6862aa6
Kent Inverarity <kent@nowhere>
2023-11-30 13:40:51 +1030

    Update versioning

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name="aseg_gdf2",
-    packages=["aseg_gdf2"],
-    version="0.3.0",
-    description="Python code to help read ASEG GDF2 packages ",
+    packages=("aseg_gdf2", ),
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="Python package to help read ASEG GDF2 packages ",
     long_description=open("README.md", mode="r").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/kinverarity1/aseg_gdf2",
