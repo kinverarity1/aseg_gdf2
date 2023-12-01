a5f46171c35594d458bc9fddf9ffc0331df69bae
Kent Inverarity <kent@nowhere>
2023-12-01 14:18:54 +1030

    Update

--- setup.py
+++ setup.py
@@ -13,8 +13,14 @@ setup(
     author_email="kinverarity@hotmail.com",
     license="MIT",
     classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
     ),
-    keywords="science geophysics",
+    keywords="python geophysics file-formats",
     install_requires=("pandas", "dask"),
 )
