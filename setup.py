513f1da19e51cef8283a5798d666dc64f43ce4a2
Callum McKenna <callum.mckenna@ga.gov.au>
2026-02-10 16:19:22 +1100

    Update setup.py
    - Require Python 3.9 or higher
    - Add pandas and dask version constraints

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(
     name="aseg_gdf2",
-    packages=("aseg_gdf2",),
+    packages=["aseg_gdf2"],
+    python_requires=">=3.9",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     description="Python package to help read ASEG GDF2 packages ",
@@ -12,7 +13,7 @@ setup(
     author="Kent Inverarity",
     author_email="kinverarity@hotmail.com",
     license="MIT",
-    classifiers=(
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
@@ -20,7 +21,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Topic :: Scientific/Engineering",
-    ),
+    ],
     keywords="python geophysics file-formats",
-    install_requires=("pandas", "dask"),
+    install_requires=("pandas>=2.0,<3.0", "dask>=2023.1.0,<2025.0.0"),
 )
